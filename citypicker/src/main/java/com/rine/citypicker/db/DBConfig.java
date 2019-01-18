8e741edd5086373a71c6426844b6faa7ba09c0a7
rine <475337316@qq.com>
2019-01-18 14:31:16 +0800

    地区数据库修改

--- citypicker/src/main/java/com/rine/citypicker/db/DBConfig.java
+++ citypicker/src/main/java/com/rine/citypicker/db/DBConfig.java
@@ -7,8 +7,8 @@ package com.rine.citypicker.db;
  */
 public class DBConfig {
     //V1和V2在每次更新数据库时，调换，还要将assets的名字更换为LATEST_DB_NAME的名字相同
-    public static final String DB_NAME_V1 = "china_cities.db";
-    public static final String DB_NAME_V2 = "china_cities_v2.db";
+    public static final String DB_NAME_V1 = "china_cities_v2.db";
+    public static final String DB_NAME_V2 = "china_cities.db";
     public static final String LATEST_DB_NAME = DB_NAME_V2;
 
     public static final String TABLE_NAME = "city";
